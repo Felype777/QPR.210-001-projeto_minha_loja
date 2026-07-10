0df52d6b28346650e1209973be3f63f04eaed30e
Felype Gabrel <felype.g.ferreira@aluno.senai.br>
2026-07-10 14:13:38 -0300

    formatado os arquivso

--- js/produtos.js
+++ js/produtos.js
@@ -16,23 +16,34 @@ const listarProdutos = () => {
         imgCard.setAttribute('src', elem.caminho_imagem)
         imgCard.setAttribute('alt', elem.descricao_produto)
     
+        //CRIANDO O ELEMENTO P E ATRIBUINDO A DESCRIÇÃO DOS PRODUTOS
         const pCard = document.createElement('p')
         pCard.innerHTML = elem.descricao_produto
-    
+
+         //CRIANDO O ELEMENTO h2 E ATRIBUINDO O VALOR UNITÁRIODEIXANDO EM DUAS CASAS DECIMAIS E SUBSTITUINDO PONTO POR VÍRGULA
         const h2Card = document.createElement('h2')
-        h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.', ',')}`
-    
-        const btnCard = document.createElement('button')
-        btnCard.setAttribute('class', 'btn-add')
+        h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario)
+        .toFixed(2).replace('.', ',')}`
+
+         //CRIANDO O ELEMENTO button E DEFININDO OS ATRIBUTOS CLASS E A DESCRIÇÃO ADICIONAR
+         const btnCard = document.createElement('button')
+         btnCard.setAttribute('class', 'btn-add')
         btnCard.innerHTML = 'Adicionar'
 
+        //ADICIONANDO OS ELEMENTOS FILHOS AOS divCard
         divCard.appendChild(imgCard)
         divCard.appendChild(pCard)
         divCard.appendChild(h2Card)
         divCard.appendChild(btnCard)
 
+        //ADICIONANDO O divCard A SECTION CARDS
         sectionCards.appendChild(divCard)
-    
     })
 
 }
+
+//CHAMANDO A FUNÇÃO listarProdutos
+listarProdutos()
+
+//MONTANDO OS MENUS SEÇÕES
+cosnt 
